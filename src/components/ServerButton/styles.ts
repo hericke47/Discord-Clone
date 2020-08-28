8601fcc76f383ca2ba76be37f5d1814df682181a
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-28 16:17:47 -0300

    server list finished

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -40,7 +40,26 @@ export const Button = styled.button<Props>`
   }
 
   &::after {
+    background-color: var(--notification);
+    width: auto;
+    height: 16px;
 
+    padding: 0 4px;
+
+    position: absolute;
+    bottom: -4px;
+    right: -4px;
+
+    border-radius: 12px;
+    border: 4px solid var(--quaternary);
+
+    text-align: right;
+    font-size: 13px;
+    font-weight: bold;
+    color: var(--white);
+
+    content: '${props => props.mentions && props.mentions}';
+    display: ${props => props.mentions && props.mentions > 0 ? 'inline' : 'none'}
   }
 
   transition: border-radius .2s, background-color .2s;
