7340a12c9671f6a6d316441c070bb45a96cc1fda
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-28 19:30:47 -0300

    discord completo

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef])
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage 
+          key={n}
+          author="Herick Exterkoetter"
+          date="09/01/2020"
+          content="Hoje é o meu aniversário!"
+        />
+        ))}
+        
+      <ChannelMessage 
+          author="DevSpace"
+          date="09/01/2020"
+          content={
+            <>
+              <Mention>@Herick Exterkoetter</Mention>, OI herick
+            </>
+          }
+          hasMention
+          isBot
+        />
+
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #chat-livre" />
